c55bb36a22a3d98af791dd730d0447661d006474
Leekyoungwook <lkw001110code@gmail.com>
2025-03-10 14:11:15 +0900

    footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -10,23 +10,22 @@ const Footer = () => {
 
 
   return (
-    <footer className="flex justify-center items-center pt-1 sm:pt-10 md:pt-8 border-t">
-      <div className="flex justify-between flex-wrap container mx-auto px-4">
+    <footer className="flex justify-center items-center border-t w-full h-[auto] ">
+      <div className="">
+      <div className="flex justify-between flex-wrap w-full h-full px-4">
         {/* 로고 및 주소 정보 */}
-        <div className="py-2 md:py-4">
-          <div className="mb-2 lg:mb-0">
-            <img
-              src={AnifarmLogo}
-              alt="Logo"
-              className="w-[30px] md:w-[50px] mb-2"
-            />
-            <p className="text-xs md:text-sm text-gray-600">
-              (08503) 서울 금천구 가산디지털2로 144 현대테라타워 가산DK A동 20층
-              &nbsp;
-              <br className="hidden md:block" />
-              대표전화: 02-2038-0800 | FAX: 02-000-0000
-            </p>
-          </div>
+        <div className="py-2 md:py-4 flex items-center">
+          <img
+            src={AnifarmLogo}
+            alt="Logo"
+            className="w-[30px] md:w-[50px] mb-2 mr-2"
+          />
+          <p className="text-xs md:text-sm text-gray-600">
+            (08503) 서울 금천구 가산디지털2로 144 현대테라타워 가산DK A동 20층
+            &nbsp;
+            <br className="hidden md:block" />
+            대표전화: 02-2038-0800 | FAX: 02-000-0000
+          </p>
         </div>
 
         {/* 버튼 추가 */}
@@ -39,10 +38,11 @@ const Footer = () => {
 
         {/* 저작권 정보 */}
         <div className="py-1 md:py-4 text-center">
-          <p className="text-xs md:text-sm text-gray-500">
+          <p className="text-xs md:text-sm text-gray-500 mt-12">
             Copyright © 2025 AnIfarmAll rights reserved.
           </p>
         </div>
+      </div>
       </div>
     </footer>
   );
