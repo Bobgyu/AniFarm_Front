fd4bd11790c3c58e5e1411a0f3fb27e39189348f
Bobgyu <bobgyu0422@gmail.com>
2025-03-11 10:20:29 +0900

    HeaderFooter

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -10,39 +10,36 @@ const Footer = () => {
 
 
   return (
-    <footer className="flex justify-center items-center border-t w-full h-[auto] ">
-      <div className="w-2/3">
-      <div className="flex justify-between flex-wrap w-full h-full px-4">
-        {/* 로고 및 주소 정보 */}
-        <div className="py-2 md:py-4 flex items-center">
-          <img
-            src={AnifarmLogo}
-            alt="Logo"
-            className="w-[30px] md:w-[50px] mb-2 mr-2"
-          />
-          <p className="text-xs md:text-sm text-gray-600">
-            (08503) 서울 금천구 가산디지털2로 144 현대테라타워 가산DK A동 20층
-            &nbsp;
-            <br className="hidden md:block" />
-            대표전화: 02-2038-0800 | FAX: 02-000-0000
-          </p>
-        </div>
+    <footer className="flex justify-center items-center border-t w-full h-24">
+      <div className="w-full max-w-7xl px-4">
+        <div className="flex justify-between items-center w-full h-full">
+          {/* 로고 및 주소 정보 */}
+          <div className="flex items-center">
+            <img
+              src={AnifarmLogo}
+              alt="Logo"
+              className="w-[50px] mr-4"
+            />
+            <p className="text-sm text-gray-600">
+              (08503) 서울 금천구 가산디지털2로 144 현대테라타워 가산DK A동 20층
+              &nbsp;
+              <br className="hidden md:block" />
+              대표전화: 02-2038-0800 | FAX: 02-000-0000
+            </p>
+          </div>
 
-        {/* 버튼 추가 */}
-        <div className="py-2 md:py-4 hover:text-green-500">
-          <button onClick={navigateToTest4}>Test4</button>
-        </div>
+          {/* 버튼 추가 */}
+          <div className="flex items-center hover:text-green-500">
+            <button onClick={navigateToTest4}>Test4</button>
+          </div>
 
-        {/* 구분선 */}
-        <div className="border-t border-gray-300 my-2 md:my-4"></div>
-
-        {/* 저작권 정보 */}
-        <div className="py-1 md:py-4 text-center">
-          <p className="text-xs md:text-sm text-gray-500 mt-12">
-            Copyright © 2025 AnIfarmAll rights reserved.
-          </p>
+          {/* 저작권 정보 */}
+          <div className="text-center">
+            <p className="text-sm text-gray-500">
+              Copyright © 2025 AnIfarmAll rights reserved.
+            </p>
+          </div>
         </div>
-      </div>
       </div>
     </footer>
   );
